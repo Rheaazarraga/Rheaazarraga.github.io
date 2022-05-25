035a33599ee7b810457d936aacf68d251b5dc814
Rhea <rheaazarraga@gmail.com>
2022-05-25 13:09:07 -0700

    add scroll back to top button with functionality

--- src/app.js
+++ src/app.js
@@ -6,3 +6,23 @@ function toggleButton() {
 }
 
 hamburgerButton.addEventListener('click', toggleButton);
+
+// get the back to top button
+scrollBtn = document.getElementById('scroll-btn');
+
+// when user scrolls 20 px down from the top of the document, show the back to top button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = 'block';
+  } else {
+    scrollBtn.style.display = 'none';
+  }
+}
+
+// when user clicks on button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // safari
+  document.documentElement.scrollTop = 0; // chrome, firefox, IE
+}
